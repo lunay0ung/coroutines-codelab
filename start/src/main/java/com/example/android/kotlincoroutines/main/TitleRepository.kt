7d07d1adf60d8c1cd2c2cb1c973000bb18a4b8ca
Soyoung Luna Yang <luna.ensocorro@gmail.com>
2020-08-11 21:51:02 +0900

    8. Making main-safe functions from blocking code

--- start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
+++ start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
@@ -19,7 +19,10 @@ package com.example.android.kotlincoroutines.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
 import com.example.android.kotlincoroutines.util.BACKGROUND
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import okhttp3.Dispatcher
 
 /**
  * TitleRepository provides an interface to fetch a title or request a new one be generated.
@@ -43,11 +46,45 @@ class TitleRepository(val network: MainNetwork, val titleDao: TitleDao) {
     val title: LiveData<String?> = titleDao.titleLiveData.map { it?.title }
 
 
-    // TODO: Add coroutines-based `fun refreshTitle` here
+    // Without introducing coroutines to the network or database,
+    // we can make this code main-safe using coroutines.
+    // This will let us get rid of the callback and allow us to pass the result back to the thread that initially called it.
+    //You can use this pattern anytime you need to do blocking or CPU intensive work
+    // from inside a coroutine such as sorting and filtering a large list or reading from disk.
     //코루틴 버전의 refreshTitle 함수
+    //코루틴을 사용하면 콜백이 없이도 + 해당 콜백을 호출한 스레드로 바로 결과값을 전달할 수 있다
+    //이 패턴은 blocking한 작업 혹은 CPU intensie한 작업에 언제나 사용할 수 있다
+    //가령 큰 사이즈의 리스트를 sorting, filtering하거나 디스크에서 읽어오는 경우이다.
     suspend fun refreshTitle() {
-        //TODO network로부터 새로고침하고 디비에 쓴다
-        delay(500)
+        //dispatcher 간 switch를 위해서 코루틴은 withContext를 사용한다.
+        //withContext를 호출하면, 다른 dispatcher로 switch된 후 해당 disptcher를 호출한 dispatcher로 lambda의 결과를 갖고 되돌아온다
+
+        //기본적으로 코루틴은 세 가지 Dispatchers를 제공한다: Main, IO, 그리고 Default다.
+        //IO dispatcher는 network나 disk로 부터 읽는 IO작업에, Default dispatcher는 CPU intensive한 작업에 최적화되어있다.
+
+        //이 코드는 여전히 blocking call을 사용한다: execute(), insertTitles() 모두 코루틴이 실행 중인 스레드를 블락할 것이다.
+        //하지만 withContext를 통해 Dispatchers.IO로 switch되면서, 우리는 IO Dispatcher 내부의 스레드 하나를 블락킹하게 된다.
+        //이 작업을 호출한 코루틴은 아마도 Dispatchers.Main 상에서 실행되고 있을 것이며, withContext lambda가 완료될 때까지 suspend될 것이다.
+        //콜백 버전과 비교하면, 두 가지 중요한 차이점이 있다.
+        //1) withContext는 결과값을 withContext를 부른 Dispatcher로 반환한다(이 경우에는 .Main)
+        // -> 콜백버전의 경우에는 BACKGROUND executor service 내부 스레드 상의 콜백을 호출했다
+        //2) caller는 이 함수에 callback을 전달할 필요가 없다. 결과값이나 에러를 받기 위해 suspend / resume될 뿐이다.
+
+        withContext(Dispatchers.IO) {
+           val result = try {
+                network.fetchNextTitle().execute()
+           } catch (cause: Throwable) {
+               throw TitleRefreshError("unable to refresh title", cause)
+           }
+
+           if (result.isSuccessful) {
+               // Save it to database
+               titleDao.insertTitle(Title(result.body()!!))
+           } else {
+               // If it's not successful, inform the callback of the error
+               throw TitleRefreshError("Unable to refresh title", null)
+           }
+       }
     }
 
 
@@ -59,10 +96,13 @@ class TitleRepository(val network: MainNetwork, val titleDao: TitleDao) {
      */
     fun refreshTitleWithCallbacks(titleRefreshCallback: TitleRefreshCallback) {
         // This request will be run on a background thread by retrofit
-        BACKGROUND.submit {
+        BACKGROUND.submit { //작업을 위해 BACKGROUND 스레드로 switch한다
+            //callback에 기초한 이 함수는 메인스레드를 블락하지 않기 때문에 main-safe하다
+            //하지만 콜러에게 작업이 완료되었음을 알리기 위해 콜백을 사용해야 하며,
+            //BACKGROUND thread 상의 콜백을 호출해야 한다
             try {
                 // Make network request using a blocking call
-                val result = network.fetchNextTitle().execute()
+                val result = network.fetchNextTitle().execute() //execute-> blocking method
                 if (result.isSuccessful) {
                     // Save it to database
                     titleDao.insertTitle(Title(result.body()!!))
